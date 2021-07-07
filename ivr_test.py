6c1f1c155685eb51ce730602eb7faccd0852877c
dferreira-ds <66534246+dferreira-ds@users.noreply.github.com>
2021-07-07 15:56:14 -0500

    Add files via upload

---
+++ ivr_test.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+from twilio.twiml.voice_response import VoiceResponse, Gather
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def message():
+    resp = VoiceResponse()
+    gather = Gather(timeout=5, numDigits='1', action='/voice')
+    gather.say('For sales, press 1, For support, press 2.')
+    resp.append(gather)
+    #resp.redirect('/voice')
+    return str(resp)
+
+@app.route("/voice", methods=['GET', 'POST'])
+def voice():
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # If Twilio's request to our app included already gathered digits,
+    # process them
+    if 'Digits' in request.values:
+        # Get which digit the caller chose
+        choice = request.values['Digits']
+
+        # <Say> a different message depending on the caller's choice
+        if choice == '1':
+            resp.say('You selected sales. Good for you!')
+            return str(resp)
+        elif choice == '2':
+            resp.say('You need support. We will help!')
+            return str(resp)
+        else:
+            # If the caller didn't choose 1 or 2, apologize and ask them again
+            resp.say("Sorry, I don't understand that choice.")
+
+    # Start our <Gather> verb
+    #gather = Gather(num_digits=1)
+    #gather.say('For sales, press 1. For support, press 2.')
+    #resp.append(gather)
+
+    # If the user doesn't select an option, redirect them into a loop
+    resp.redirect('/voice')
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
